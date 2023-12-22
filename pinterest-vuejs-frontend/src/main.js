620a79bc091b9e48e6310affef802c2889f9f0e0
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-12-22 10:55:20 +0700

    up

--- pinterest-vuejs-frontend/src/main.js
+++ pinterest-vuejs-frontend/src/main.js
@@ -1,3 +1,4 @@
+import 'virtual:windi.css'
 import './assets/main.css'
 
 import { createApp } from 'vue'
--- pinterest-vuejs-frontend/vite.config.js
+++ pinterest-vuejs-frontend/vite.config.js
@@ -4,14 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import nightwatchPlugin from 'vite-plugin-nightwatch'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    nightwatchPlugin(),
-  ],
+  plugins: [vue(), vueJsx(), nightwatchPlugin(), WindiCSS()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
