a05cd0911cf406284c192ff5551cf92c318fcf15
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-12-23 09:08:19 +0700

    up

--- pinterest-nestjs-backend/ormconfig.js
+++ pinterest-nestjs-backend/ormconfig.js
@@ -1,7 +1,8 @@
 module.exports = {
   host: process.env.DATABASE_HOST,
-  type: DATABASE_TYPE,
+  type: process.env.DATABASE_TYPE,
   port: process.env.DATABASE_PORT,
+  sid: process.env.DATABASE_SID,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_DB_NAME,
