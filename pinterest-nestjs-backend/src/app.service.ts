3a7fb240d6d486ccf7e686141ca4f8556b415733
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-12-23 21:42:57 +0700

    up

--- pinterest-nestjs-backend/src/app.service.ts
+++ pinterest-nestjs-backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'This is picturest API project!';
+    return 'This is Picturest API project!';
   }
 }
