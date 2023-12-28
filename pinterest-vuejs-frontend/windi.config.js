623bebaaaebfa3cc15ee145b805f8012c86ee9a3
Long Phan (Idol Dev) <lordaizensousuke2001@gmail.com>
2023-12-28 21:55:30 +0700

    remove vue

--- pinterest-vuejs-frontend/windi.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'windicss/helpers'
-
-export default defineConfig({
-  extract: {
-    include: ['src/**/*.{vue,html,jsx,tsx,js,ts}'],
-    exclude: ['node_modules', '.git']
-  },
-  attributify: true
-})
